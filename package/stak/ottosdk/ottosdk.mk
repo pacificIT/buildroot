49417e2285637d26e5c5c934c8db59e31977c6c1
Wynter Woods <zerotri@gmail.com>
2015-01-22 22:42:04 -0800

    Removed create_ap install from ottosdk package

--- package/stak/ottosdk/ottosdk.mk
+++ package/stak/ottosdk/ottosdk.mk
@@ -4,35 +4,28 @@
 #
 #############################################################
 
-OTTOSDK_VERSION = CES
-# OTTOSDK_SITE = $(call github,NextThingCo,otto-sdk,$(OTTOSDK_VERSION))
-OTTOSDK_SITE = file://$(TOPDIR)/package/stak/ottosdk
-OTTOSDK_SOURCE = otto-sdk-$(OTTOSDK_VERSION).tar.gz
+
+OTTOSDK_VERSION = ces-fastcamd
+OTTOSDK_SITE = git@github.com:NextThingCo/otto-sdk.git
+OTTOSDK_SITE_METHOD = git
+
 OTTOSDK_LICENSE = MIT
 OTTOSDK_LICENSE_FILES = LICENCE
 OTTOSDK_DEPENDENCIES = libbcm2835
 
-#	TARGET_ENV = \
-#		CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
-#	    LDFLAGS="-L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/opt/vc/lib/" \
-#	    INCLUDES="-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/opt/vc/include -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/opt/vc/include/interface/vcos/pthreads"
-
-#	define OTTOSDK_BUILD_CMDS
-#	    $(TARGET_ENV) $(MAKE)  -C $(@D) all
-#	endef
-
 define OTTOSDK_INSTALL_STAGING_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/stak-test $(STAGING_DIR)/usr/bin/stak-test
+	$(INSTALL) -D -m 0755 $(@D)/stak-test $(STAGING_DIR)/usr/bin/stak-test
 endef
 
 define OTTOSDK_INSTALL_TARGET_CMDS
+#	git --git-dir=$(@D)/.git --work-tree=$(@D) submodule init
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/fastcmd
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk/assets
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk/fastcmd
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk/create_ap
+#	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk/create_ap
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk/python
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi/otto-sdk/python/views
 
@@ -40,11 +33,11 @@ define OTTOSDK_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -D -m 0755 $(@D)/fastcmd/* $(TARGET_DIR)/home/pi/fastcmd
 	$(INSTALL) -D -m 0755 $(@D)/fastcmd/* $(TARGET_DIR)/home/pi/otto-sdk/fastcmd
-	$(INSTALL) -D -m 0755 $(@D)/vendor/tools/create_ap/* $(TARGET_DIR)/home/pi/otto-sdk/create_ap
+#	$(INSTALL) -D -m 0755 $(@D)/vendor/tools/create_ap/* $(TARGET_DIR)/home/pi/otto-sdk/create_ap
 	$(INSTALL) -D -m 0755 $(@D)/python/server.py $(TARGET_DIR)/home/pi/otto-sdk/python
 	$(INSTALL) -D -m 0755 $(@D)/python/views/* $(TARGET_DIR)/home/pi/otto-sdk/python/views
 
-    $(INSTALL) -D -m 0755 $(@D)/assets/* $(TARGET_DIR)/home/pi/otto-sdk/assets
+	$(INSTALL) -D -m 0755 $(@D)/assets/* $(TARGET_DIR)/home/pi/otto-sdk/assets
 endef
 
 $(eval $(generic-package))
--- package/stak/platform/raspberrypi/ottoupdate/ottoupdate.mk
+++ package/stak/platform/raspberrypi/ottoupdate/ottoupdate.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-OTTOUPDATE_VERSION = test2
-OTTOUPDATE_SITE = file://$(TOPDIR)/package/stak/platform/raspberrypi/ottoupdate
-OTTOUPDATE_SOURCE = otto-update-$(OTTOUPDATE_VERSION).tar.gz
+OTTOUPDATE_VERSION = 9a3e6d86b3c3f143a4eb3b12a6b461e2649348df
+OTTOUPDATE_SITE = git@github.com:NextThingCo/otto-update.git
+OTTOUPDATE_SITE_METHOD = git
+# OTTOUPDATE_SITE = file://$(TOPDIR)/package/stak/platform/raspberrypi/ottoupdate
+# OTTOUPDATE_SOURCE = otto-update-$(OTTOUPDATE_VERSION).tar.gz
 OTTOUPDATE_LICENSE = GPLv2
 OTTOUPDATE_LICENSE_FILES = LICENCE
 
