71b1f81dd67629c0ab7e1bc537a0f81c8dd77aeb
Wynter Woods <zerotri@gmail.com>
2015-04-09 04:34:20 +0000

    Update otto-boot package, rootfs customizations and udev

--- board/raspberrypi/stak/customize_rootfs.sh
+++ board/raspberrypi/stak/customize_rootfs.sh
@@ -10,8 +10,11 @@ CRYPTEDPASS=$(perl -e 'print crypt("doge1234","salt")')
 sudo sed -i -e "s#^root:[^:]*:#root:$CRYPTEDPASS:#" ${TARGET_DIR}/etc/shadow
 # echo "/dev/mmcblk0p4          /updates         vfat    defaults   0      0" | sudo tee --append ${TARGET_DIR}/etc/fstab
 
-echo "allow-hotplug eth1" | sudo tee --append ${TARGET_DIR}/etc/network/interfaces
-echo "iface eth1 inet dhcp" | sudo tee --append ${TARGET_DIR}/etc/network/interfaces
+sed -e "s/auto \(eth.*\)/allow-hotplug \1/g" ${TARGET_DIR}/etc/network/interfaces
+
+#echo "allow-hotplug eth1" | sudo tee --append ${TARGET_DIR}/etc/network/interfaces
+#echo "iface eth1 inet dhcp" | sudo tee --append ${TARGET_DIR}/etc/network/interfaces
 # sudo install -m 775 ${BUILD_ROOT}/board/raspberrypi/stak/root/etc/init.d/S01launch-otto ${TARGET_DIR}/etc/init.d
 # sudo install -m 775 ${BUILD_ROOT}/board/raspberrypi/stak/root/etc/init.d/S03loadmodules   ${TARGET_DIR}/etc/init.d
 # sudo install -m 775 ${BUILD_ROOT}/board/raspberrypi/stak/root/etc/init.d/*   ${TARGET_DIR}/etc/init.d
+sudo install -m 775 ${BUILD_ROOT}/board/raspberrypi/stak/root/etc/udev/rules.d/*   ${TARGET_DIR}/etc/udev/rules.d
