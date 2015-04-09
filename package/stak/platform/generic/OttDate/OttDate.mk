11a1e5537bb5999117292afcad36b45121268815
Alex Kaplan <kaplan2539@gmail.com>
2015-04-09 12:20:17 -0700

    OttDate: added dependency to openssl

--- package/stak/platform/generic/OttDate/OttDate.mk
+++ package/stak/platform/generic/OttDate/OttDate.mk
@@ -7,6 +7,6 @@ OTTDATE_VERSION = v0.0.3
 OTTDATE_SITE = $(call github,NextThingCo,OttDate,$(OTTDATE_VERSION))
 OTTDATE_INSTALL_STAGING = YES
 OTTDATE_INSTALL_TARGET = YES
-#OTTDATE_DEPENDENCIES = host-libaaa libbbb
+OTTDATE_DEPENDENCIES = host-openssl openssl
 
 $(eval $(cmake-package))
