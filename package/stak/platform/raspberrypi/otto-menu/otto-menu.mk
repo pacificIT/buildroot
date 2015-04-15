959102568a4baa19f86a6ac8ab25357e2f1c16ad
Wynter Woods <zerotri@gmail.com>
2015-04-15 00:35:01 +0000

    Update for otto-menu

--- package/stak/platform/raspberrypi/otto-menu/otto-menu.mk
+++ package/stak/platform/raspberrypi/otto-menu/otto-menu.mk
@@ -13,13 +13,13 @@ OTTO_MENU_INSTALL_STAGING = NO
 OTTO_MENU_INSTALL_TARGET = YES
 OTTO_MENU_CONF_OPTS = -DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -march=armv6 -mfloat-abi=hard" -DVC_SDK="$(STAGING_DIR)/opt/vc"
 
-# define OTTO_MENU_GIT_SUBMODULE_FIXUP
-# 	git clone git@github.com:sansumbrella/Choreograph.git $(@D)/deps/Choreograph
-#   git clone git@github.com:alecthomas/entityx.git $(@D)/deps/entityx
-#   git clone git@github.com:NextThingCo/otto-gfx.git $(@D)/deps/otto-gfx
-#   git clone git@github.com:NextThingCo/otto-runner.git $(@D)/deps/otto-runner
-# 	git clone git@github.com:NextThingCo/otto-utils.git $(@D)/deps/otto-utils
-# endef
+define OTTO_MENU_GIT_SUBMODULE_FIXUP
+	git clone git@github.com:sansumbrella/Choreograph.git $(@D)/deps/Choreograph
+  git clone git@github.com:alecthomas/entityx.git $(@D)/deps/entityx
+  git clone git@github.com:NextThingCo/otto-gfx.git $(@D)/deps/otto-gfx
+  git clone git@github.com:NextThingCo/otto-runner.git $(@D)/deps/otto-runner
+	git clone git@github.com:NextThingCo/otto-utils.git $(@D)/deps/otto-utils
+endef
 
 define OTTO_MENU_INSTALL_TARGET_CMDS
   $(INSTALL) -d 0644 $(TARGET_DIR)/stak/sdk
@@ -28,7 +28,7 @@ define OTTO_MENU_INSTALL_TARGET_CMDS
   $(INSTALL) -m 0755 $(@D)/assets/* $(TARGET_DIR)/stak/sdk/assets
 endef
 
-# OTTO_MENU_POST_DOWNLOAD_HOOKS += OTTO_MENU_GIT_SUBMODULE_FIXUP
+OTTO_MENU_POST_DOWNLOAD_HOOKS += OTTO_MENU_GIT_SUBMODULE_FIXUP
 
 
 $(eval $(cmake-package))
