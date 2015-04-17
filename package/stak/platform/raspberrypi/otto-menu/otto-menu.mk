a11911e3d8f46c08cdc482e7f8e7ed4dd5046b35
Alex Kaplan <kaplan2539@gmail.com>
2015-04-16 18:33:13 -0700

    build OttDate & otto-menu from master, also using cmake find_package

--- package/stak/platform/raspberrypi/otto-menu/otto-menu.mk
+++ package/stak/platform/raspberrypi/otto-menu/otto-menu.mk
@@ -4,14 +4,15 @@
 #
 #############################################################
 
-OTTO_MENU_VERSION = 90d5e3aceacdfd2c535008fb51a8e097cebebc4a
+#OTTO_MENU_VERSION = 90d5e3aceacdfd2c535008fb51a8e097cebebc4a
+OTTO_MENU_VERSION = master
 OTTO_MENU_SITE = git@github.com:NextThingCo/otto-menu.git
 OTTO_MENU_SITE_METHOD = git
 OTTO_MENU_LICENSE = GPLv2
 OTTO_MENU_LICENSE_FILES = LICENCE
 OTTO_MENU_INSTALL_STAGING = NO
 OTTO_MENU_INSTALL_TARGET = YES
-OTTO_MENU_CONF_OPTS = -DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -march=armv6 -mfloat-abi=hard" -DVC_SDK="$(STAGING_DIR)/opt/vc"
+OTTO_MENU_CONF_OPTS = -DCMAKE_PREFIX_PATH=$(STAGING_DIR)/usr/lib/CMake -DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -march=armv6 -mfloat-abi=hard" -DVC_SDK="$(STAGING_DIR)/opt/vc"
 
 define OTTO_MENU_GIT_SUBMODULE_FIXUP
 	git clone git@github.com:sansumbrella/Choreograph.git $(@D)/deps/Choreograph
@@ -29,6 +30,5 @@ define OTTO_MENU_INSTALL_TARGET_CMDS
 endef
 
 OTTO_MENU_POST_DOWNLOAD_HOOKS += OTTO_MENU_GIT_SUBMODULE_FIXUP
-
 
 $(eval $(cmake-package))
