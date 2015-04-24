daf226f1a9394d71e9d0ea6b552b7c97a93f4742
Alex Kaplan <kaplan2539@gmail.com>
2015-04-24 16:34:49 -0700

    otto-network-setup integration: fixes

--- package/stak/platform/raspberrypi/otto-network-setup/otto-network-setup.mk
+++ package/stak/platform/raspberrypi/otto-network-setup/otto-network-setup.mk
@@ -4,13 +4,14 @@
 #
 #############################################################
 
-OTTO_NETWORK_SERVICE_VERSION = master
-OTTO_NETWORK_SERVICE_SITE = $(call github,NextThingCo,otto-network-setup,$(OTTO_NETWORK_SERVICE_VERSION))
-OTTDATE_INSTALL_TARGET = YES
-OTTO_NETWORK_SERVICE_LICENSE = MIT
-OTTO_NETWORK_SERVICE_LICENSE_FILES = LICENCE
+OTTO_NETWORK_SETUP_VERSION = master
+OTTO_NETWORK_SETUP_SITE = git@github.com:NextThingCo/otto-network-setup.git
+OTTO_NETWORK_SETUP_SITE_METHOD = git
+OTTO_NETWORK_SETUP_INSTALL_TARGET = YES
+OTTO_NETWORK_SETUP_LICENSE = MIT
+OTTO_NETWORK_SETUP_LICENSE_FILES = LICENSE
 
-define OTTO_NETWORK_SERVICE_INSTALL_INIT_SYSTEMD
+define OTTO_NETWORK_SETUP_INSTALL_INIT_SYSTEMD
 
 	$(INSTALL) -D -m 755 $(@D)/otto-network-setup.py \
 		$(TARGET_DIR)/usr/bin/otto-network-setup.py
@@ -27,19 +28,19 @@ define OTTO_NETWORK_SERVICE_INSTALL_INIT_SYSTEMD
   $(INSTALL) -D -m 644 $(@D)/bootstrap/css/bootstrap.min.css \
 		$(TARGET_DIR)/usr/lib/otto-network-setup/boostrap/css/bootstrap.min.css
 
-  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.woff2
+  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.woff2 \
 		$(TARGET_DIR)/usr/lib/otto-network-setup/boostrap/fonts/glyphicons-halflings-regular.woff2
 
-  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.svg
+  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.svg \
 		$(TARGET_DIR)/usr/lib/otto-network-setup/boostrap/fonts/glyphicons-halflings-regular.svg
 
-  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.woff
+  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.woff \
 		$(TARGET_DIR)/usr/lib/otto-network-setup/boostrap/fonts/glyphicons-halflings-regular.woff
 
-  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.ttf
+  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.ttf \
 		$(TARGET_DIR)/usr/lib/otto-network-setup/boostrap/fonts/glyphicons-halflings-regular.ttf
 
-  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.eot
+  $(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.eot \
 		$(TARGET_DIR)/usr/lib/otto-network-setup/boostrap/fonts/glyphicons-halflings-regular.eot
 
 	$(INSTALL) -D -m 644 $(@D)/otto-network-setup.service \
