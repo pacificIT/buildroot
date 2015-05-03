3753a8080617bdcabe7a82d1a7a3b1deae6cfcb0
Alex Kaplan <kaplan2539@gmail.com>
2015-05-03 16:51:01 -0700

    otto-network-setup.mk adapted to new structure (assets/views)

--- package/stak/platform/raspberrypi/otto-network-setup/otto-network-setup.mk
+++ package/stak/platform/raspberrypi/otto-network-setup/otto-network-setup.mk
@@ -16,65 +16,62 @@ define OTTO_NETWORK_SETUP_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 755 $(@D)/otto-network-setup.py \
 		$(TARGET_DIR)/usr/bin/otto-network-setup.py
 
-	$(INSTALL) -D -m 644 $(@D)/setup.tpl \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/setup.tpl
+	$(INSTALL) -D -m 644 $(@D)/views/setup.tpl \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/views/setup.tpl
 
-	$(INSTALL) -D -m 644 $(@D)/images.tpl \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/images.tpl
+	$(INSTALL) -D -m 644 $(@D)/views/images.tpl \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/views/images.tpl
 
-	$(INSTALL) -D -m 644 $(@D)/jquery-1.11.2.min.js \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/jquery-1.11.2.min.js
+	$(INSTALL) -D -m 644 $(@D)/assets/jquery-1.11.2.min.js \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/jquery-1.11.2.min.js
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/js/bootstrap.js \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/js/bootstrap.min.js
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/js/bootstrap.js \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/js/bootstrap.min.js
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/css/bootstrap.min.css \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/css/bootstrap.min.css
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/css/bootstrap.min.css \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/css/bootstrap.min.css
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.woff2 \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/fonts/glyphicons-halflings-regular.woff2
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/fonts/glyphicons-halflings-regular.woff2 \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/fonts/glyphicons-halflings-regular.woff2
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.svg \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/fonts/glyphicons-halflings-regular.svg
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/fonts/glyphicons-halflings-regular.svg \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/fonts/glyphicons-halflings-regular.svg
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.woff \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/fonts/glyphicons-halflings-regular.woff
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/fonts/glyphicons-halflings-regular.woff \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/fonts/glyphicons-halflings-regular.woff
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.ttf \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/fonts/glyphicons-halflings-regular.ttf
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/fonts/glyphicons-halflings-regular.ttf \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/fonts/glyphicons-halflings-regular.ttf
 
-	$(INSTALL) -D -m 644 $(@D)/bootstrap/fonts/glyphicons-halflings-regular.eot \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/bootstrap/fonts/glyphicons-halflings-regular.eot
+	$(INSTALL) -D -m 644 $(@D)/assets/bootstrap/fonts/glyphicons-halflings-regular.eot \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/bootstrap/fonts/glyphicons-halflings-regular.eot
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/photoswipe-ui-default.min.js \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/photoswipe-ui-default.min.js
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/photoswipe-ui-default.min.js \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/photoswipe-ui-default.min.js
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/photoswipe.min.js \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/photoswipe.min.js
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/photoswipe.min.js \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/photoswipe.min.js
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/photoswipe.js \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/photoswipe.js
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/photoswipe.js \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/photoswipe.js
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/photoswipe.css \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/photoswipe.css
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/photoswipe.css \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/photoswipe.css
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/photoswipe-ui-default.js \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/photoswipe-ui-default.js
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/photoswipe-ui-default.js \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/photoswipe-ui-default.js
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/default-skin \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/default-skin
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/default-skin/default-skin.css \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/default-skin/default-skin.css
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/default-skin/default-skin.css \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/default-skin/default-skin.css
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/default-skin/default-skin.svg \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/default-skin/default-skin.svg
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/default-skin/default-skin.svg \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/default-skin/default-skin.svg
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/default-skin/default-skin.png \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/default-skin/default-skin.png
 
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/default-skin/default-skin.png \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/default-skin/default-skin.png
-
-	$(INSTALL) -D -m 644 $(@D)/PhotoSwipe/default-skin/preloader.gif \
-		$(TARGET_DIR)/usr/lib/otto-network-setup/PhotoSwipe/default-skin/preloader.gif
+	$(INSTALL) -D -m 644 $(@D)/assets/PhotoSwipe/default-skin/preloader.gif \
+		$(TARGET_DIR)/usr/lib/otto-network-setup/assets/PhotoSwipe/default-skin/preloader.gif
 
 	$(INSTALL) -D -m 644 $(@D)/otto-network-setup.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/otto-network-setup.service
